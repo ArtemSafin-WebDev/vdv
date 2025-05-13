0052032a2ca9c9edff45d02e605b1b70ced5c537
Artem Safin <artemsafin.webdev@gmail.com>
2025-05-13 04:50:22 +0300

    Шапка с неавторизованным юзером

---
+++ pages-data/homeUnauthorized.js
@@ -0,0 +1,7 @@
+const homeUnauthorized = {
+  "/index-unauthorized.html": {
+    title: "Главная страница",
+  },
+};
+
+export default homeUnauthorized;
--- pages.config.js
+++ pages.config.js
@@ -14,6 +14,7 @@ import orders from "./pages-data/orders";
 import checkout from "./pages-data/checkout";
 import search from "./pages-data/search";
 import cart from "./pages-data/cart";
+import homeUnauthorized from "./pages-data/homeUnauthorized";
 const pagesConfig = {
   ...home,
   ...news,
@@ -31,6 +32,7 @@ const pagesConfig = {
   ...checkout,
   ...search,
   ...cart,
+  ...homeUnauthorized,
 };
 
 export default pagesConfig;
